aee85023c5870ab706c39b671ed554b085589cea
Hoang Pham <hp@hellofresh.com>
2017-08-24 17:51:03 +0200

    Add Login

---
+++ setting_get_started.sh
@@ -0,0 +1,5 @@
+curl -X POST -H "Content-Type: application/json" -d '{
+  "get_started":{
+    "payload":"GET_STARTED_PAYLOAD"
+  }
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAByZB4xoKnwBABCzC5P9bbavBLpjMYQc61r4J1d01EPexiQZCnrcUDa0PmTrCzjHOU0EXztXhaTCZCilBOCt8RZAkZB8YeTOU85W9unEunz6yJk70cNVZCYvv3orBN416XRGR8JCOe4HSMOGDv368FoAszE41OkpmZA23qcDEoFgZDZD"
